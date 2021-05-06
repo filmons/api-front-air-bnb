80ac574355d539c0a940ad5295853a6e51bb06d5
OmniaGomaa88 <minna1172018@gmail.com>
2021-05-06 16:04:46 +0200

    inscription

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './pages/Home.js'
-import DetailsPlace from './pages/DetailsPlace'
+import Home from './pages/Home.js';
+import DetailsPlace from './pages/DetailsPlace';
+import SignUp from './pages/SignUp';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
      <Switch>
       <Route exact path='/' component={Home} />
       <Route exact path='/place/:id' component={DetailsPlace}/>
+      <Route exact path='/signup' component={SignUp} />
       </Switch>
     </Router>
   );
---
+++ src/pages/SignUp.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import axios from 'axios';
+
+class SignUp extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            prenom: '',
+            nom: '',
+            email: '',
+            role: '',
+            password: ''
+        }
+
+    }
+
+    handleChange = (event) => {
+        this.setState({ 
+            [event.target.name]: event.target.value 
+        })
+    }
+
+    handleClick = () => {
+        const options = {
+            email: this.state.email,
+            password: this.state.password,
+            first_name: this.state.prenom,
+            last_name: this.state.nom,
+            role: this.state.role
+        }
+
+        axios.post('http://localhost:8000/api/signup', options, {
+            headers: {'content-type': 'application/json'}
+        })
+        .then(data => console.log(data))
+        .catch(error => console.log(error))
+        console.log(options);
+    }
+    
+    render() {
+        return (
+            <>
+               <section>
+                   <div className="container">
+                        <div className="form">
+                            <p>
+                                <label htmlFor="">Prénom :</label>
+                                <input name="prenom" type="text" onChange={this.handleChange} />
+                            </p>
+                            <p>
+                                <label htmlFor="">Nom :</label>
+                                <input name="nom" type="text" onChange={this.handleChange} />
+                            </p>
+                            <p>
+                                <label htmlFor="">Émail :</label>
+                                <input name="email" type="email" onChange={this.handleChange} />
+                            </p>
+                            <p>
+                                <label htmlFor="">Rôle :</label>
+                                <select name="role" onChange={this.handleChange} >
+                                    <option >Votre Rôle</option>
+                                    <option value="0">Touriste</option>
+                                    <option value="1">Hôte</option>
+                                </select>
+                            </p>
+                            <p>
+                                <label htmlFor="">Mot de passe :</label>
+                                <input name="password" type="password" onChange={this.handleChange} />
+                            </p>
+                            <p>
+                                <button onClick={this.handleClick}>Inscription</button>
+                            </p>
+                        </div>
+                   </div>
+                </section> 
+            </>
+        )
+    }
+}
+
+export default SignUp;
