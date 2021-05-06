4c75d9d26cc82fe294868a82180a6aa94547a5d0
OmniaGomaa88 <minna1172018@gmail.com>
2021-05-06 17:37:40 +0200

    api page login

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home.js';
 import DetailsPlace from './pages/DetailsPlace';
 import SignUp from './pages/SignUp';
+import Login from './pages/Login';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route exact path='/' component={Home} />
       <Route exact path='/place/:id' component={DetailsPlace}/>
       <Route exact path='/signup' component={SignUp} />
+      <Route exact path='/login' component={Login} />
       </Switch>
     </Router>
   );
